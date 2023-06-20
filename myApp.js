f61e02d9830f6b377771e541275dc0006fec6702
Topeetee <temitopeakinwekomi28@gmail.com>
2023-06-20 13:25:19 -0700

    completed mongodb and mongoose course on freecodecamp

--- myApp.js
+++ myApp.js
@@ -1,54 +1,183 @@
 require('dotenv').config();
+const mongoose = require("mongoose");
+require('dotenv').config();
 
+
+
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 let Person;
+const personSchema = new mongoose.Schema({
+  name: {
+    type: String
+  },
+  age: {
+    type: Number
+  },
+  favoriteFoods: {
+    type: [String]
+  }
+});
+Person = mongoose.model('Person', personSchema);
 
 const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+  function savePerson() {
+    const person = new Person({
+      name: "temitope",
+      age: 37,
+      favoriteFoods: ["yam", "rice"]
+    });
+
+    person.save(function(err, savedPerson) {
+      if (err) {
+        console.log(err);
+        done(err); // Pass the error to the 'done' callback
+      } else {
+        console.log("Saved Person:", savedPerson);
+        done(null, savedPerson); // Pass the saved person to the 'done' callback
+      }
+    });
+  }
+
+  savePerson();
 };
 
+// Example usage:
+createAndSavePerson(function(err, savedPerson) {
+  if (err) {
+    console.log("Error:", err);
+  } else {
+    console.log("Saved Person:", savedPerson);
+  }
+});
+
+
+
+
+const arrayOfPeople = [
+  {
+    name: "tope",
+    age: 37,
+    favoriteFoods: ["beans", "rice"]
+  },{
+    name:"tolu",
+    age:26,
+    favoriteFoods:["panCake","shawarma"]
+  }
+]
 const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+  Person.create(arrayOfPeople)
+    .then((createdPeople) => {
+      done(null, createdPeople);
+    })
+    .catch((error) => {
+      done(error);
+    });
 };
+
 
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({ name: personName })
+    .then((foundPeople) => {
+      done(null, foundPeople);
+    })
+    .catch((error) => {
+      done(error);
+    });
 };
 
 const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+  Person.findOne({ favoriteFoods: food }, (error, foundfood) => {
+    if (error) {
+      return done(error);
+    }
+    done(null, foundfood);
+  });
 };
 
+
+
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById({_id:personId}) .then((foundId) => {
+    done(null, foundId);
+  })
+  .catch((error) => {
+    done(error);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
   const foodToAdd = "hamburger";
-
-  done(null /*, data*/);
+  Person.findById(personId)
+    .then((foundPerson) => {
+      foundPerson.favoriteFoods.push(foodToAdd);
+      foundPerson.markModified("favoriteFoods");
+      return foundPerson.save();
+    })
+    .then((updatedPerson) => {
+      done(null, updatedPerson);
+    })
+    .catch((error) => {
+      done(error);
+    });
 };
+
 
 const findAndUpdate = (personName, done) => {
   const ageToSet = 20;
+    const updateOptions = { new: true };
+    const update = { age: 20 };
+  
+    Person.findOneAndUpdate({ name: personName }, {age:ageToSet}, { new: true })
+      .then((updatedPerson) => {
+        done(null, updatedPerson);
+      })
+      .catch((error) => {
+        done(error);
+      });
 
-  done(null /*, data*/);
 };
 
 const removeById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findByIdAndRemove(personId).then((removeId)=>[
+    done(null,removeId)
+  ]).catch((error)=>{
+    done(error);
+  })
+
 };
 
 const removeManyPeople = (done) => {
   const nameToRemove = "Mary";
 
-  done(null /*, data*/);
+    const query = { name: { $in: nameToRemove } };
+  
+    Person.remove(query, (error, result) => {
+      if (error) {
+        done(error);
+      } else {
+        done(null, result);
+      }
+    });
+  
 };
 
 const queryChain = (done) => {
   const foodToSearch = "burrito";
-
-  done(null /*, data*/);
+  Person.find()
+  .where("favoriteFoods")
+  .in([foodToSearch])       
+  .skip()                   
+  .sort({ name: 1 })
+  .limit(2)
+  .select({ age: 0 })
+  .exec((error,querysorted)=>{
+    if(error){
+      done(error)
+    }else{
+      done(null,querysorted);
+    }
+  })
 };
 
 /** **Well Done !!**
